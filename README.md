f3e30313a99f31498a95a743a35eb9a3e5604830
positionexchanger <positionexchange.holder@gmail.com>
2022-04-05 00:37:49 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Make subgraphs never die 🥲
+
+**This repository was born because of Subgraph archive policy**
+
+```
+A subgraph is defined as "inactive" if it was deployed to the Hosted Service more than 45 days ago, and if it has received 0 queries in the last 30 days.
+```
---
+++ script.js
@@ -0,0 +1,42 @@
+const { default: axios } = require('axios')
+
+const APIs = [
+  'https://api.thegraph.com/subgraphs/name/gafranslotteria/position-token',
+  'https://api.thegraph.com/subgraphs/name/gafranslotteria/position-nft',
+  'https://api.thegraph.com/subgraphs/name/gafranslotteria/position-referral',
+  'https://api.thegraph.com/subgraphs/name/gafranslotteria/position-busd-vault',
+  'https://api.thegraph.com/subgraphs/name/gafranslotteria/position-bnb-vault',
+  'https://api.thegraph.com/subgraphs/name/gafranslotteria/position-bond-01',
+  'https://api.thegraph.com/subgraphs/name/gafranslotteria/position-bond-02',
+  'https://api.thegraph.com/subgraphs/name/gafranslotteria/position-bond-03',
+  'https://api.thegraph.com/subgraphs/name/gafranslotteria/position-public-sale',
+  'https://api.thegraph.com/subgraphs/name/gafranslotteria/nft-reward-pool',
+  'https://api.thegraph.com/subgraphs/name/gafranslotteria/position-staking-pool'
+]
+
+const script = async () => {
+  try {
+    APIs.forEach(async api => {
+      const res = await axios.post(api, {
+        query: `
+          {
+            _meta {
+              block {
+                number
+              }
+            }
+          }
+        `
+      }, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      console.log(res.data?.data._meta.block.number + ':' + api)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+script()
